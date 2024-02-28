cda097463c25238d312018575d4c9b7ab2617937
WH1TE-SHAD0W <marek@culak.sk>
2024-02-28 22:03:41 +0000

    ca5e1 code

---
+++ CA/5/exercise_1.py
@@ -0,0 +1,14 @@
+def verify_email(email: str):
+    email = email.split('@')
+    if len(email) == 2:
+        if len(email[0]) > 0:
+            email[1] = email[1].split('.')
+            if len(email[1]) >= 2:
+                domain_not_empty_check: bool = True
+                for domain in email[1]:
+                    if len(domain) == 0:
+                        domain_not_empty_check = False
+                if domain_not_empty_check:
+                    if len(email[1][-1]) == 2:
+                        return True
+    return False
