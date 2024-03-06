85868592f5a9cc3c21d55c38fd32cf96f2795b7a
WH1TE-SHAD0W <marek@culak.sk>
2024-03-06 13:44:58 +0000

    hot fixes of email verification

--- CA/5/exercise_1.py
+++ CA/5/exercise_1.py
@@ -9,6 +9,6 @@ def verify_email(email: str):
                     if len(domain) == 0:
                         domain_not_empty_check = False
                 if domain_not_empty_check:
-                    if len(email[1][-1]) == 2:
+                    if len(email[1][-1]) >= 2:
                         return True
     return False
